145b3aae8c4834cfb47db9aea78ed5bbef088404
AdrienVictor <46904762+AdrienVictor@users.noreply.github.com>
2019-04-11 09:39:29 +0300

    update before pull

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 // **** flip function ****
 const cards = document.querySelectorAll('.profile-card');
 function flipCard() {
+    console.log(this);
+
+    console.log(this.dataset);
+
     this.classList.toggle('flip');
 }
 cards.forEach(card => card.addEventListener('click', flipCard));
